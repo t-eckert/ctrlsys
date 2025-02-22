4e8aed2c8cf591030dec14434c8e5172b89d78f1
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-02-22 12:37:53 -0500

    Add Rust workspace

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+/Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/target/debug/cli: /Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/src/main.rs
---
+++ cli/target/debug/deps/cli-a5d0ff37b27fcb37.d
@@ -0,0 +1,5 @@
+/Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/target/debug/deps/cli-a5d0ff37b27fcb37: src/main.rs
+
+/Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/target/debug/deps/cli-a5d0ff37b27fcb37.d: src/main.rs
+
+src/main.rs:
