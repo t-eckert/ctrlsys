c88a1751333dcd5674fffe6d7009b712d3d21b42
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-02-22 13:10:38 -0500

    Remove the old target for cli

--- cli/target/debug/cli.d
+++
@@ -1,1 +0,0 @@
-/Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/target/debug/cli: /Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/src/main.rs
--- cli/target/debug/deps/cli-a5d0ff37b27fcb37.d
+++
@@ -1,5 +0,0 @@
-/Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/target/debug/deps/cli-a5d0ff37b27fcb37: src/main.rs
-
-/Users/thomaseckert/Repos/github.com/t-eckert/ctrlsys/cli/target/debug/deps/cli-a5d0ff37b27fcb37.d: src/main.rs
-
-src/main.rs:
