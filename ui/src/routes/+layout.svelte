677dbba77736becbd75ec2358e98059f4103510d
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-09-17 22:53:21 -0400

    Delete existing UI

--- ui/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import '../app.css';
-	let { children } = $props();
-</script>
-
-<div class="min-h-screen bg-zinc-50">
-	{@render children()}
-</div>
--- ui/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import ExchangeRate from '$lib/tiles/exchange-rate.svelte';
-</script>
-
-<div class="grid grid-cols-8 gap-2 p-1">
-	<ExchangeRate />
-</div>
